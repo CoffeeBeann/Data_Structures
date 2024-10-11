960c7453cff2a60536bffe6ab2bdd11e83dbd602
CoffeeBeann <m261194@usna.edu>
2024-10-11 09:41:58 -0400

    hw07

--- HW/hw07/DoubleTree.java
+++ HW/hw07/DoubleTree.java
@@ -6,6 +6,7 @@ To Implement anv AVL Tree of Doubles
 
 // Import libraries
 import java.util.NoSuchElementException;
+import java.lang.Math.*;
 
 public class DoubleTree implements AddMax 
 {
@@ -17,25 +18,14 @@ public class DoubleTree implements AddMax
         private int height;
         private Node left;
         private Node right;
-        private Node parent;
         
-        // Constructor method w/o arguments 
-        public Node() 
-        {
-            height = -1;
-            left = null;
-            right = null;
-            parent = null;
-        }
-
         // Constructor method w/ arguements
-        public Node(double d, int h, Node l, Node r, Node p)
+        public Node(double d)
         {
             this.data = d;
-            this.height = h;
-            this.left = l;
-            this.right = r;
-            this.parent = p;
+            this.height = 0;
+            this.left = null;
+            this.right = null;
         }
     }
 
@@ -44,6 +34,29 @@ public class DoubleTree implements AddMax
     
     // AVL constructor method 
     public DoubleTree() { root = null; }
+    
+    /**
+     * Method to return the balance factor of a Node
+     */
+    public int balanceFact(Node curr) 
+    {
+        // Left child with no right child
+        if (curr.left != null && curr.right == null)
+            return 0;
+
+        // Right child with no left child
+        if (curr.left == null && curr.right != null)
+            return 0;
+
+        // Both children
+        if (curr.left != null && curr.right != null)
+            return 0;
+
+        // No children (Leaf node)
+        else
+            return 0;
+    }
+
 
     /**
      * Methods to add a double to the AVL Tree
@@ -53,41 +66,75 @@ public class DoubleTree implements AddMax
 
     public Node addRecursive(Node curr, double x) 
     {
-        // Height variable for rebalance calculations
-        int leftH, rightH;
-
-        // Adding first Node to AVL Tree
+        // Adding Node to AVL Tree
         if (curr == null) 
-            return new Node(x, 0, null, null, null);
+            return new Node(x);
+        
+        // If duplicate is found, make no changes
+        if (x == curr.data)
+            return curr;
 
         // Traverse down left side if x < curr
         else if (x < curr.data) 
-        {
-            if (curr.left == null) 
-                curr.left =  new Node(x, curr.height++, null, null, curr);
-            else
-                return addRecursive(curr.left, x);
-        }
-
+            curr.left = addRecursive(curr.left, x);
+        
         // Traverse down right side if x > curr
-        else if (x > curr.data) 
-        {
-            if (curr.right == null) 
-                curr.right = new Node(x, curr.height++, null, null, curr);
-            else
-                return addRecursive(curr.right, x);
-        }
-        
-        // If duplicate is found, make no changes
-        else 
-            return this.root;
+        else    
+            curr.right = addRecursive(curr.right, x);
+    
+        // Update height of Node
+        updateHeight(curr);
 
-        // Check if tree is balanced and perform rotations as needed
-        
-        if (curr.left.height - curr.right.height)
-        
-
+        return curr;
     }
+
+    /**
+     * Left rotation method
+     */
+    public Node lRotate(Node oldRoot) 
+    {
+        Node newRoot = oldRoot.right;
+        Node middle = newRoot.left;
+        newRoot.left = oldRoot;
+        oldRoot.right = middle;
+        return newRoot;
+    }
+
+    /**
+     * Right rotation method
+     */
+    public Node rRotate(Node oldRoot) 
+    {
+        Node newRoot = oldRoot.left;
+        Node middle = newRoot.right;
+        newRoot.right = oldRoot;
+        oldRoot.left = middle;
+        return newRoot;
+    }
+
+    /**
+     * Method to update heights throughout an AVL Tree
+     */
+    public void updateHeight(Node curr)
+    {
+        // Left child with no right child
+        if (curr.left != null && curr.right == null)
+            curr.height = curr.left.height++;
+      
+        // Right child with no left child
+        if (curr.left == null && curr.right != null)
+            curr.height = curr.right.height++;
+        
+        // Both children
+        if (curr.left != null && curr.right != null)
+            curr.height = Math.max(curr.left.height, curr.right.height) + 1;
+        
+        // No children (Leaf node)
+        else
+            curr.height = 0;
+    }
+
+
 
     /**
      * Method to remove the max element from an AVL Tree
@@ -103,10 +150,12 @@ public class DoubleTree implements AddMax
 
     public double removeRecurse(Node curr) throws NoSuchElementException 
     {
+        double max;
+
         if (curr.right == null) 
         {
             // Save max double
-            double max = curr.data;
+            max = curr.data;
 
             // Leaf Node hit
             if (curr.left == null) 
@@ -116,27 +165,10 @@ public class DoubleTree implements AddMax
             
             return max;
         }
-
-        return removeRecurse(curr.right);
-    }
-    
-    /**
-     * Main method for testing
-     */
-    public static void main(String [] args) 
-    {
-        // AVL declaration
-        DoubleTree tree = new DoubleTree();
-        tree.add(5);
-        tree.add(10);
-        tree.add(3);
-        tree.add(7);
-        tree.add(15);
-        for (int i = 0; i < 5; i++)
-            tree.removeMax();
-
-        tree.removeMax();
-    }
+        
+        // Update height before returning 
+        max = removeRecurse(curr.right);
+        updateHeight(curr);
+        return max;
+    }    
 }
-
-
