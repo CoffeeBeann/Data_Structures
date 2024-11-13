2668261767320c30aba17e8b9fbaefeed1f487a7
CoffeeBeann <m261194@usna.edu>
2024-11-13 14:16:40 -0500

    syning Ic312

--- HW/hw09/CycleFinder.java
+++ HW/hw09/CycleFinder.java
@@ -1,21 +1,30 @@
-public class CycleFinder {
-  public static boolean hasCycle(Graph graph, String start) {
-    // TODO you fill in this function
-    return false;
-  }
+/****************************************
+Filename: CycleFinder.java
+Author: MIDN Ian Coffey (m261194)
+To Determine if a Node is in a Cycle
+****************************************/
 
-  public static void main(String[] args) {
-    // XXX you shouldn't need to change anything here
-    String dotf = Stdin.input("graph file: ");
-    Graph graph = DotReader.readFrom(dotf);
-
-    while (true) {
-      String start = Stdin.input("starting node or 'quit': ");
-      if (start == null || start.equals("quit")) break;
-      if (hasCycle(graph, start))
-        System.out.format("%s is in a cycle.\n", start);
-      else
-        System.out.format("%s is NOT in a cycle.\n", start);
+public class CycleFinder 
+{
+    public static boolean hasCycle(Graph graph, String start) {
+        // TODO you fill in this function
+        return false;
     }
-  }
+
+    public static void main(String[] args) 
+    {
+        // XXX you shouldn't need to change anything here
+        String dotf = Stdin.input("graph file: ");
+        Graph graph = DotReader.readFrom(dotf);
+
+        while (true) 
+        {
+            String start = Stdin.input("starting node or 'quit': ");
+            if (start == null || start.equals("quit")) break;
+            if (hasCycle(graph, start))
+                System.out.format("%s is in a cycle.\n", start);
+            else
+                System.out.format("%s is NOT in a cycle.\n", start);
+        }
+    }
 }
--- HW/hw09/Stdin.java
+++ HW/hw09/Stdin.java
@@ -1,3 +1,10 @@
+/****************************************
+Filename: STDIN.java
+Author: MIDN Ian Coffey (m261194)
+Read Stdin Line by Line
+****************************************/
+
+// Import Library
 import java.io.Console;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,29 +13,33 @@ import java.io.InputStreamReader;
  * The method to use is Stdin.input("Prompt? ")
  * which returns the next line from standard in.
  */
-public class Stdin {
-  private static Console console = null;
-  private static BufferedReader reader = null;
+public class Stdin 
+{
+    private static Console console = null;
+    private static BufferedReader reader = null;
 
-  /** Read a line of text from standard in.
-   * If input is from a terminal, the prompt is presented.
-   * Otherwise it just reads a line and ignores the prompt.
-   * @return The next line from stdin, or null if EOF is reached.
-   */
-  public static String input(String prompt) {
-    if (console == null && reader == null) {
-      // first run, set up either console or reader
-      console = System.console();
-      if (console == null)
-        reader = new BufferedReader(new InputStreamReader(System.in));
+    /** Read a line of text from standard in.
+    * If input is from a terminal, the prompt is presented.
+    * Otherwise it just reads a line and ignores the prompt.
+    * @return The next line from stdin, or null if EOF is reached.
+    */
+    public static String input(String prompt) 
+    {
+        if (console == null && reader == null) 
+        {
+            // first run, set up either console or reader
+            console = System.console();
+            if (console == null)
+                reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+        if (console != null)
+            return console.readLine(prompt);
+        try 
+        {
+            return reader.readLine();
+        }
+            catch (java.io.IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-    if (console != null)
-      return console.readLine(prompt);
-    try {
-      return reader.readLine();
-    }
-    catch (java.io.IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
 }
