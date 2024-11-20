c9b666ea25334df7627f928d93bf1ebe920b67a4
CoffeeBeann <m261194@usna.edu>
2024-11-19 22:14:06 -0500

    sync

--- HW/hw09/CycleFinder.java
+++ HW/hw09/CycleFinder.java
@@ -4,28 +4,39 @@ Author: MIDN Ian Coffey (m261194)
 To Determine if a Node is in a Cycle
 ****************************************/
 
+// Import Libraries
 import java.util.*;
 
 public class CycleFinder 
 {
+    /**
+     * Method to check if a node is in a cycle
+     */
     public static boolean hasCycle(Graph graph, String start) 
     {
+        // Create sets and stack for visisted and fringe list
         Set<String> visited = new HashSet<>();
         Stack fringe = new Stack();
 
+        // Push start to the stack
         fringe.push(start);
         
+        // Traverse through fringe
         while (!fringe.empty()) 
         {
+            // Grab next object off stack
             String uu = fringe.pop().toString();
 
+            // Check if we have not visited uu
             if (!visited.contains(uu)) 
             {
+                // Add to stack
                 visited.add(uu);
                 for (String vv : graph.neighbors(uu)) 
                 {
                     fringe.push(vv);
-
+                    
+                    // Cycle found
                     if (vv.equals(start))
                         return true;
                 }
